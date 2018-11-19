654b489ab21857e72dd7934aeb60b665b2588edf
Nat Baylon <nbaylon@healytics.com>
2018-11-19 15:23:28 +0100

    added untested rotation translations

---
+++ src/util/wideTurns.js
@@ -0,0 +1,146 @@
+export {
+	R: {
+		equivalentMove: "l",
+		rotation: "x"
+	},
+	"R'": {
+		equivalentMove: "l'",
+		rotation: "x'"
+	},
+	"R2": {
+		equivalentMove: "l2",
+		rotation: "x2"
+	},
+	U: {
+		equivalentMove: "d",
+		rotation: "y"
+	},
+	"U'": {
+		equivalentMove: "d'",
+		rotation: "y'"
+	},
+	"U2": {
+		equivalentMove: "d2",
+		rotation: "y2"
+	},
+	L: {
+		equivalentMove: "r",
+		rotation: "x'"
+	},
+	"L'": {
+		equivalentMove: "r'",
+		rotation: "x"
+	},
+	"L2": {
+		equivalentMove: "r2",
+		rotation: "x2"
+	},
+	D: {
+		equivalentMove: "u",
+		rotation: "y'"
+	},
+	"D'": {
+		equivalentMove: "u'",
+		rotation: "y"
+	},
+	"D2": {
+		equivalentMove: "u2",
+		rotation: "y2"
+	},
+	F: {
+		equivalentMove: "b",
+		rotation: "z"
+	},
+	"F'": {
+		equivalentMove: "b'",
+		rotation: "z'"
+	},
+	"F2": {
+		equivalentMove: "b2",
+		rotation: "z2"
+	},
+	B: {
+		equivalentMove: "f",
+		rotation: "z'"
+	},
+	"B'": {
+		equivalentMove: "f'",
+		rotation: "z'"
+	},
+	"B2": {
+		equivalentMove: "f2",
+		rotation: "z2"
+	},
+	r: {
+		equivalentMove: "L",
+		rotation: "x"
+	}
+	"r'": {
+		equivalentMove: "L'",
+		rotation: "x'"
+	}
+	"r2": {
+		equivalentMove: "L2",
+		rotation: "x2"
+	}
+	u: {
+		equivalentMove: "D",
+		rotation: "y"
+	}
+	"u'": {
+		equivalentMove: "D'",
+		rotation: "y'"
+	}
+	"u2": {
+		equivalentMove: "D2",
+		rotation: "y2"
+	}
+	l: {
+		equivalentMove: "R ",
+		rotation: "x'"
+	}
+	"l'": {
+		equivalentMove: "R'",
+		rotation: "x"
+	}
+	"l2": {
+		equivalentMove: "R2",
+		rotation: "x2"
+	}
+	d: {
+		equivalentMove: "U ",
+		rotation: "y'"
+	}
+	"d'": {
+		equivalentMove: "U'",
+		rotation: "y"
+	}
+	"d2": {
+		equivalentMove: "U2",
+		rotation: "y2"
+	}
+	f: {
+		equivalentMove: "B ",
+		rotation: "z"
+	}
+	"f'": {
+		equivalentMove: "B'",
+		rotation: "z'"
+	}
+	"f2": {
+		equivalentMove: "B2",
+		rotation: "z2"
+	}
+	b: {
+		equivalentMove: "F",
+		rotation: "z'"
+	}
+	"b'": {
+		equivalentMove: "F'",
+		rotation: "z"
+	}
+	"b2": {
+		equivalentMove: "F2",
+		rotation: "z2"
+	}
+}
