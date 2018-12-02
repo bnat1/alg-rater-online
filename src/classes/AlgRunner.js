d4aea25be3b13ae4db2ba028f0a1fc308f8d95dd
Nat Baylon <nbaylon@healytics.com>
2018-12-02 12:44:01 +0100

    updated alg

--- src/classes/AlgRunner.js
+++ src/classes/AlgRunner.js
@@ -5,24 +5,21 @@ export default class AlgRunner {
 	}
 
 	// returns an array of algs with their scores
-	run(alg, candidateExecutions=[], maxAllowedCandidates=10, currentScore=0, hands = new Hands(/*TODO customize turnign preference*/)) {
-
-		// do first move with hands, and get rating for the move
-		// add rating to current score
-		
-		// depth control
-		// if currentScore > score of the last candidate
-			// return
-
+	run(alg, currentIndex, candidateExecutions=[], maxAllowedCandidates=10, currentScore=0, hands=new Hands(/*TODO customize turnign preference*/)) {
 		// if there are no moves left in the alg
 			// insert alg into candidates array in sorted position, maintaining the length of the array
 			// return
 		
+		// do first move with hands, and get rating for the move
+		// add rating to current score
+
+		// depth control
+		// if currentScore > score of the last candidate
+			// return
+
 		// run as is
 		// run with wide turn as first move (if possible)
 		// run with slice applied (if possible)
 		// run with rotations (I can see this getting out of hand if rotations pile up, but depth control should help this)
-
-
 	}
 }
