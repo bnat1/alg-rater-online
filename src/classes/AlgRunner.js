5a84658db3f5bf308bada08a58ca3236ccc6427a
Nat Baylon <bnat1@umbc.edu>
2018-12-03 14:58:08 +0100

    working on hands

--- src/classes/AlgRunner.js
+++ src/classes/AlgRunner.js
@@ -1,6 +1,7 @@
 import rotations from '../util/rotations'
 const allRotations = Object.keys(rotations)
 import Alg from './Alg'
+import Hands from './Hands'
 
 export default class AlgRunner {
 
@@ -8,12 +9,8 @@ export default class AlgRunner {
 
 	}
 
-	// runIfApplicable(, remainingMoves, currentCandidate, candidateExecutions=[], maxAllowedCandidates=10, hands) {
-
-	// }
-
 	// returns an array of algs with their scores
-	run(remainingMoves, currentCandidate, candidateExecutions=[], maxAllowedCandidates=10, hands=new Hands(/*TODO customize turnign preference*/)) {
+	run(remainingMoves, currentCandidate, candidateExecutions=[], maxAllowedCandidates=10, hands=new Hands()) {
 		// insert execution into candidates if there are no more turns to do
 		if (!remainingMoves.length) {
 			if (!candidateExecutions.length) {
@@ -28,35 +25,41 @@ export default class AlgRunner {
 			}
 			return
 		}
-		
-		// TODO: something isn't right here. The first move of an alg will never be transformed.
-		// do first move with hands, and get score for the move
-		// add score to current candidate's score
-		const moveScore = hands.turn(remainingMoves[0])
-		currentCandidate.moves.push(remainingMoves[0])
-		currentCandidate.score += moveScore
 
+		// do first move and judge it
+		// this is still wrong. the first turn will never be transformed. 
+		const { score, nextMoves, singleExecutedMove } = hands.doTurn(remainingMoves)
+		currentCandidate.moves = [...currentCandidate.moves, ...singleExecutedMove] 
+		currentCandidate.score += score
+		// TODO: run
+		
 		// search depth control
-		if (currentCandidate.score > candidateExecutions[candidateExecutions.length - 1].score) {
+		if (currentCandidate.score > candidateExecutions[maxAllowedCandidates.length - 1].score) {
 			return
 		}
-		// run starting with next move as is
-		// TODO: make better deep copy of current candidate
-		run([...remainingMoves.slice(1)], [...currentCandidate], candidateExecutions, maxAllowedCandidates, hands)
 
-		// run with wide turn as next move (if possible)
-		const remainingMovesWideTurn = Alg.applyWideTurn(remainingMoves.slice(1))
-		if (remainingMovesWideTurn[0] !== remainingMoves[1]) {
-			run([...remainingMoves.slice(1)], [...currentCandidate], candidateExecutions, maxAllowedCandidates, hands)
+		// run with wide turn as next move
+		const remainingMovesWideTurn = Alg.applyWideTurn(nextMoves)
+		if (remainingMovesWideTurn[0] !== nextMoves[0]) {
+			// TODO: this should be doTurn and then run
+			run([...remainingMovesWideTurn], currentCandidate, candidateExecutions, maxAllowedCandidates, hands)
 		}
 
 		// run with slice as next move (if possible)
-		run([...remainingMoves.slice(1)], [...currentCandidate], candidateExecutions, maxAllowedCandidates, hands)
-		
-		// run with rotations as next move (I can see this getting out of hand. TODO: Rotations together should be treated as one move to be done by hands)
-		allRotations.forEach(rotation => run([...remainingMoves.slice(1)], [...currentCandidate], candidateExecutions, maxAllowedCandidates, hands)
+		const remainingMovesSlice = Alg.applySlice(nextMoves)
+		if (remainingMovesSlice[0] !== nextMoves[0]) {
+			// TODO: this should be doTurn and then run
+			run([...remainingMovesSlice], currentCandidate, candidateExecutions, maxAllowedCandidates, hands)
+		}
 
+		// run with rotations as next move
+		// todo: prevent infinite recursion
+		allRotations.forEach(rotation => {
+			const remainingMovesRotation = rotateMoves(rotation, nextMoves)
+			// TODO: this should be doTurn and then run
+			run([rotation, ...remainingMovesRotation], currentCandidate, candidateExecutions, maxAllowedCandidates, hands)
+		})
+
+		return candidateExecutions
 	}
 }
-
-// R U R' U'
